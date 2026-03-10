f372b300db949c6173b2bef86927af0cdea544b7
MUHAMMADULLAH1991 <muhammadullahsaify859658@gmail.com>
2026-03-10 08:11:07 +0600

    Blog Create is Done

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -16,5 +17,31 @@ class BlogController extends Controller
     public function createBlog()
     {
         return View('admin.blog.create');
+    }
+
+    public function storeBlog(Request $request)
+    {
+        $blog = new Blog();
+
+        $blog->title = $request->title;
+        $blog->subtitle = $request->subtitle;
+        $blog->author_name = $request->author_name;
+        $blog->blog_details = $request->blog_details;
+       
+        if(isset($request->image)){
+
+         $image = $request->file('image');
+         $imageName = rand().'.'.$image->getClientOriginalExtension();
+        //  $blog->image = $imageName;
+         $image->move('blogs',  $imageName);
+
+         $blog->image = url('blogs/'.$imageName);
+
+        }
+
+        $blog->save();
+
+        toastr()->success('Blog created successfully');
+        return redirect()->back();
     }
 }
